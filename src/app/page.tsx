93a9893811f1216834fa0b58012e0cccbd4fafd4
Faysel Abdella <fayselcode@gmail.com>
2025-03-16 09:53:40 +0300

    refactor: clean up comments and enhance code clarity in App component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,22 @@ function App() {
   const [items, setItems] = useState([]);
   const [isSynced, setIsSynced] = useState(false);
 
+  // Step 1: Create a new branch
+  // Step 2: Do tasks
+  // Step 3: Commit your changes using appropriate commit message
+  // Step 4: Push your changes to the remote repository
+  // Step 5: Create a pull request
+
+  // Bonus:
+  // - Add the type of items state
+  // - Make this root page server-side component
+
   const USER_API = "https://jsonplaceholder.typicode.com/users";
 
   // TODO: Implement function to handle sync button click
   const handleSyncData = () => {
     // ...
   };
-
-  // Bonus:
-  // - Add the type of items state
-  // - Make this root page server-side component
 
   return (
     <div className="text-center mx-auto p-4">
